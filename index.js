86fd6b0883037b7440012a1f9775eef1da794868
Lucas Nascimento Távora <38147859+tavoralucas@users.noreply.github.com>
2020-11-07 12:48:54 -0300

    Add algumas telas

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("OLA")
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 1000
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            polygon: {
+                nb_sides: 150
+            }
+        },
+        size: {
+            value: 1,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 70,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: false,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    retina_detect: true
+});
